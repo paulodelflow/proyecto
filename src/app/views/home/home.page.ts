af4471e5be96351ff6681cb0c4496eba2abf025b
paulodelflow <pa.escobarm@duocuc.cl>
2024-03-29 22:32:53 -0300

    home
    creación del home

--- src/app/views/home/home.page.ts
+++ src/app/views/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter  } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
 
 @Component({
@@ -8,6 +8,14 @@ import { IonContent } from '@ionic/angular/standalone';
   standalone: true,
   imports: [IonContent],
 })
+
 export class HomePage {
   constructor() {}
+  hola = true;
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  @Output() opcionSeleccionada = new EventEmitter<number>();
+
+  emitirOpcionSeleccionada(opcion: number) {
+    this.opcionSeleccionada.emit(opcion);
+  }
 }
